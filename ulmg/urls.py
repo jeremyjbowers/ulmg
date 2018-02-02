b3f4692cb8dd381c23fdcf1b79e60771155b809d
Jeremy Bowers <jeremyjbowers@gmail.com>
2018-02-02 16:56:06 -0500

    First round of user-facing things

--- ulmg/urls.py
+++ ulmg/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 import nested_admin
 
+from ulmg import views
+
 urlpatterns = [
     path('nested_admin/', include('nested_admin.urls')),
     path('admin/', admin.site.urls),
+    path('', views.player_list)
 ]
---
+++ ulmg/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render_to_response, get_object_or_404
+
+from ulmg import models
+
+def player_list(request):
+    context = {}
+    context['players'] = models.Player.objects.filter(active=True).order_by("team", "position", "level", "last_name")
+
+    return render_to_response('player_list.html', context=context)
