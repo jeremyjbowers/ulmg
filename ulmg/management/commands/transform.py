00ca8b572c3a63f10e38662b78391513a84bba4d
Jeremy Bowers <jeremyjbowers@gmail.com>
2019-01-20 18:40:03 -0500

    Adds some data

--- ulmg/management/commands/transform.py
+++ ulmg/management/commands/transform.py
@@ -15,6 +15,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        def write_csv(path, payload):
+            with open(path, 'w') as csvfile:
+                fieldnames = list(payload[0].keys())
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                for p in payload:
+                    writer.writerow(p)
+
         def normalize_pos(pos):
             if pos.upper() in ["1B", "2B", "3B", "SS"]:
                 pos = "IF"
@@ -24,12 +32,18 @@ class Command(BaseCommand):
                 pos = "P"
             return pos
 
-        with open('data/j2/2018-ba-j2.json', 'r') as readfile:
-            players = json.loads(readfile.read())
-
-        with open('data/j2/2018-ba-j2.csv', 'w') as writefile:
-            fieldnames = list(players[0].keys())
-            writer = csv.DictWriter(writefile, fieldnames=fieldnames)
-            writer.writeheader()
+        with open('data/mlb_draft/2019-pl-draft.html', 'r') as readfile:
+            soup = BeautifulSoup(readfile.read(), 'lxml')
+            players = soup.select('#block-ad9c042eb3db56291a08 div.sqs-block-content p')[3:]
+            payload = []
             for p in players:
-                writer.writerow(p)            
+                if len(p.select('strong')) > 0:
+                    p = p.select('strong')[0].text.strip()
+                    print(p)
+                    # p_dict = {}
+                    # p_dict['name'] = p.select('.player-name')[0].text.strip()
+                    # p_dict['mlb_rank'] = p.select('.number')[0].text.strip()
+                    # p_dict['position'] = p.select('.player-info')[0].text.strip()
+                    # payload.append(p_dict)
+
+                # write_csv('data/mlb_draft/2019-mlb-draft.csv', payload)
