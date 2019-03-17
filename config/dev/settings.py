708bd1866fa8ac323913b490c61ad52aa82daebb
Jeremy Bowers <jeremyjbowers@gmail.com>
2019-03-17 18:30:57 -0400

    change settings to roster mode

--- config/dev/settings.py
+++ config/dev/settings.py
@@ -81,8 +81,8 @@ STATIC_URL = '/static/'
 """
 Section for site-specific settings, e.g., controlling tabs, current draft year, etc.
 """
-TEAM_PROTECT_TAB = True
-TEAM_ROSTER_TAB = False
+TEAM_PROTECT_TAB = False
+TEAM_ROSTER_TAB = True
 CURRENT_SEASON = 2019
 CURRENT_SEASON_TYPE = "offseason"
 CSV_COLUMNS = [
