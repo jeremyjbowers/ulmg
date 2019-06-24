b8acba172b70385bf80ed26c7ebfce314e2e83c0
Jeremy Bowers <jeremyjbowers@gmail.com>
2019-06-24 14:32:42 -0500

    better available page

--- ulmg/views.py
+++ ulmg/views.py
@@ -191,11 +191,11 @@ def available_livestat(request):
             Q(level__in=['A', 'B'], team__isnull=True, ls_ip__gte=1, is_carded=True, position="P"))\
     .order_by('-level_order', 'last_name', 'first_name')
 
-    context['owned_hitters'] = models.Player.objects.filter(level="V", team__isnull=False, ls_plate_appearances__gte=1, is_mlb_roster=False, is_1h_c=False, is_1h_p=False, is_1h_pos=False, is_35man_roster=False, is_reserve=False).exclude(position="P").order_by('position', '-level_order', 'last_name', 'first_name')
-    context['owned_pitchers'] = models.Player.objects.filter(level="V", team__isnull=False, ls_g__gte=1, is_mlb_roster=False, is_1h_c=False, is_1h_p=False, is_1h_pos=False, is_35man_roster=False, is_reserve=False).order_by('-level_order', 'last_name', 'first_name')
+    # context['owned_hitters'] = models.Player.objects.filter(level="V", team__isnull=False, ls_plate_appearances__gte=1, is_mlb_roster=False, is_1h_c=False, is_1h_p=False, is_1h_pos=False, is_35man_roster=False, is_reserve=False).exclude(position="P").order_by('position', '-level_order', 'last_name', 'first_name')
+    # context['owned_pitchers'] = models.Player.objects.filter(level="V", team__isnull=False, ls_g__gte=1, is_mlb_roster=False, is_1h_c=False, is_1h_p=False, is_1h_pos=False, is_35man_roster=False, is_reserve=False).order_by('-level_order', 'last_name', 'first_name')
 
-    context['aa_hitters'] = models.Player.objects.filter(level="B", team__isnull=True, ls_plate_appearances__gte=1).exclude(position="P").order_by('position', '-level_order', 'last_name', 'first_name')
-    context['aa_pitchers'] = models.Player.objects.filter(level="B", team__isnull=True, ls_g__gte=1).order_by('-level_order', 'last_name', 'first_name')
+    # context['aa_hitters'] = models.Player.objects.filter(level="B", team__isnull=True, ls_plate_appearances__gte=1).exclude(position="P").order_by('position', '-level_order', 'last_name', 'first_name')
+    # context['aa_pitchers'] = models.Player.objects.filter(level="B", team__isnull=True, ls_g__gte=1).order_by('-level_order', 'last_name', 'first_name')
 
     return render(request, 'available_livestat.html', context)
 
