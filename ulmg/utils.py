0769e477e5d63a046c3df48df889fc533dcb27f7
Jeremy Bowers <jeremyjbowers@gmail.com>
2019-09-05 13:07:19 -0400

    adds advanced mode to pages

--- ulmg/utils.py
+++ ulmg/utils.py
@@ -4,16 +4,25 @@ from django.conf import settings
 
 def build_context(request):
     context = {}
-    context['roster_tab'] = settings.TEAM_ROSTER_TAB
-    context['protect_tab'] = settings.TEAM_PROTECT_TAB
+
+    # to build the nav
     context['teamnav'] = models.Team.objects.all().values('abbreviation')
-    context['value'] = False
-    if request.GET.get('value', None):
-        context['value'] = True
+
+    # for showing stats
+    context['advanced'] = False
+    if request.GET.get('adv', None):
+        context['advanced'] = True
+
+    context['roster_tab'] = settings.TEAM_ROSTER_TAB
+
+    context['protect_tab'] = settings.TEAM_PROTECT_TAB
+
+    # for search
     queries_without_page = dict(request.GET)
     if queries_without_page.get('page', None):
         del queries_without_page['page']
     context['q_string'] = "&".join(['%s=%s' % (k,v[-1]) for k,v in queries_without_page.items()])
+
     return context
 
 def str_to_bool(possible_bool):
