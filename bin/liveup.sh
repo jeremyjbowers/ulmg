64b9d35b3ccba85a004df1463ef91f11cb582fc0
Jeremy Bowers <jeremyjbowers@gmail.com>
2019-09-19 15:35:08 -0400

    stop getting new updates

--- bin/liveup.sh
+++ bin/liveup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cd /home/ubuntu/apps/ulmg; sudo su -c "source ~/.bashrc && workon ulmg && django-admin live_update" ubuntu
+# SEASON IS OVER STOP GETTING NEW STUFF
+# cd /home/ubuntu/apps/ulmg; sudo su -c "source ~/.bashrc && workon ulmg && django-admin live_update" ubuntu
