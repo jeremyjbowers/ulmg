4b3520de7f4c35d2ebb613a1260fd5d7fb200fbb
Jeremy Bowers <jeremyjbowers@gmail.com>
2020-08-16 09:40:28 -0400

    more test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,10 +4,34 @@ from ulmg import utils
 
 
 def test_normalized_pos():
-    for if_pos in ["1B", "2B", "3B", "SS"]:
-        assert utils.normalize_pos(if_pos) == "IF"
-        assert utils.normalize_pos(if_pos.lower()) == "IF"
+    for p in ["1B", "2B", "3B", "SS"]:
+        assert utils.normalize_pos(p) == "IF"
+        assert utils.normalize_pos(p.lower()) == "IF"
 
-    for of_pos in ["RF", "CF", "LF"]:
-        assert utils.normalize_pos(of_pos) == "OF"
-        assert utils.normalize_pos(of_pos.lower()) == "OF"
+    for p in ["RF", "CF", "LF"]:
+        assert utils.normalize_pos(p) == "OF"
+        assert utils.normalize_pos(p.lower()) == "OF"
+
+
+"""
+def str_to_bool(possible_bool):
+    if possible_bool:
+        if possible_bool.lower() in ["y", "yes", "t", "true"]:
+            return True
+        if possible_bool.lower() in ["n", "no", "f", "false"]:
+            return False
+    return None
+"""
+
+
+def test_str_to_bool():
+    for b in ["y", "yes", "t", "true"]:
+        assert (utils.str_to_bool(b)) == True
+        assert (utils.str_to_bool(b.upper())) == True
+
+    for b in ["n", "no", "f", "false"]:
+        assert (utils.str_to_bool(b)) == False
+        assert (utils.str_to_bool(b.upper())) == False
+
+    for b in ["nope", "yup", "yessir", 12345, True]:
+        assert (utils.str_to_bool(b)) == None
