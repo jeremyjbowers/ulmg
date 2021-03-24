0321e639a9453fbf4c8607164acf9c9bb36e88d8
Jeremy Bowers <jeremyjbowers@gmail.com>
2021-03-24 12:54:12 -0400

    loop over teams, not over an order file

--- ulmg/management/commands/generate_draft_picks.py
+++ ulmg/management/commands/generate_draft_picks.py
@@ -9,7 +9,7 @@ DRAFT_MAPS = {
 
 class Command(BaseCommand):
     """
-    django-admin generate_draft_picks 2020 midseason data/ulmg/2020-midseason-aa-order.txt
+    django-admin generate_draft_picks 2020 midseason
     this will generate the picks without saving their order.
     you can save the order with the generate_draft_order management command.
     """
@@ -17,32 +17,28 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("year", type=str)
         parser.add_argument("season", type=str)
-        parser.add_argument("draft_order_filepath", type=str)
 
     def handle(self, *args, **options):
         season = options.get("season", None)
         year = options.get("year", None)
-        draft_order_filepath = options.get("draft_order_filepath", None)
 
-        with open(draft_order_filepath, "r") as readfile:
-            order = [n for n in readfile.read().split("\n") if n != ""]
+        teams = [t for t in models.Team.objects.all()]
 
-            for o, t in enumerate(order):
-                for draft_type, rounds in DRAFT_MAPS[season]["rounds"].items():
-                    for r in range(0, rounds):
-                        draft_round = r + 1
-                        team = models.Team.objects.get(abbreviation=t)
-                        obj, created = models.DraftPick.objects.get_or_create(
-                            year=year,
-                            season=season,
-                            draft_type=draft_type,
-                            draft_round=draft_round,
-                            original_team=team,
-                        )
+        for o, team in enumerate(teams):
+            for draft_type, rounds in DRAFT_MAPS[season]["rounds"].items():
+                for r in range(0, rounds):
+                    draft_round = r + 1
+                    obj, created = models.DraftPick.objects.get_or_create(
+                        year=year,
+                        season=season,
+                        draft_type=draft_type,
+                        draft_round=draft_round,
+                        original_team=team,
+                    )
+                    obj.save()
+                    if created:
+                        obj.team = team
                         obj.save()
-                        if created:
-                            obj.team = team
-                            obj.save()
-                            print("+%s" % obj)
-                        else:
-                            print("*%s" % obj)
+                        print("+%s" % obj)
+                    else:
+                        print("*%s" % obj)
