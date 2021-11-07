c7f0225944c97ba0cddb55a20a300db8a8178085
Jeremy Bowers <jeremyjbowers@gmail.com>
2021-11-07 14:04:50 -0500

    add some more diversity to the do prod settings

--- config/do_app_platform/settings.py
+++ config/do_app_platform/settings.py
@@ -18,8 +18,10 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split("
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
+DATABASE_URL = os.environ.get("DATABASE_URL", None)
+
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    "default": dj_database_url.parse(DATABASE_URL),
 }
 
 STATIC_ROOT = "/workspace/config/staticfiles/"
