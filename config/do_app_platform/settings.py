98037f23bc8a9c193f6cdccd5da6c17948991e84
Jeremy Bowers <jeremyjbowers@gmail.com>
2021-11-07 15:04:39 -0500

    okay try this

--- config/do_app_platform/settings.py
+++ config/do_app_platform/settings.py
@@ -34,4 +34,4 @@ AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-INSTALLED_APPS = INSTALLED_APPS.append('storages')
+INSTALLED_APPS = INSTALLED_APPS + ['storages']
