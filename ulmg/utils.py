50f510bde126286c422cfeb03a7f85f436cbc0c2
Jeremy Bowers <jeremyjbowers@gmail.com>
2022-02-05 10:32:42 -0500

    Last few folks Steve identified

--- ulmg/utils.py
+++ ulmg/utils.py
@@ -924,7 +924,7 @@ def set_levels(*args, **options):
 
     print("--------- RELIEVERS A > V ---------")
     for p in models.Player.objects.filter(
-        level="A", position="P", stats__career__g__gte=301
+        level="A", position="P", stats__career__g__gte=201
     ):
         p.level = "V"
         print(p)
