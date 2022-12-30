b44331d354159510d2c4d8e5cebef7e6562ff84d
Jeremy Bowers <jeremyjbowers@gmail.com>
2022-12-30 10:43:15 -0500

    prep for adding strat obtb ratings

--- ulmg/management/commands/load_strat_ratings.py
+++ ulmg/management/commands/load_strat_ratings.py
@@ -3,84 +3,78 @@ import json
 import os
 import shutil
 import time
+import datetime
 
 from django.core.management.base import BaseCommand, CommandError
 
 from ulmg import models, utils
+# from scipy import stats
+
+import warnings
+warnings.filterwarnings("ignore")
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        self.get_pitcher_data()
-        self.load_pitchers()
+    season = None
+    hitters = None
+    hitter_obtb_l = None
+    hitter_obtb_r = None
+    hitter_obtb_tot = None
 
-        # self.get_hitter_data()
-        # self.load_hitters()
+    def handle(self, *args, **options):
+        self.season = utils.get_strat_season() + 1
+        self.load_hitters()
 
     def get_player(self, player, hitter=True):
-        name_frag = player["player"].split(",")
+        name_frag = player["player_1"].split(",")
         last = name_frag[0].strip().replace("*", "").replace("+", "")
         first = name_frag[1].strip().replace("*", "").replace("+", "")
-
-        if player["ulmg_id"].strip() != "":
-            return utils.strat_find_player(first, last, ulmg_id=player["ulmg_id"])
 
         return utils.strat_find_player(
             first, last, hitter=hitter, mlb_team_abbr=player["tm"]
         )
 
-    def get_pitcher_data(self, fresh=False):
-        if fresh:
-            os.system("rm -rf data/2022/strat_card_ratings_pitcher.json")
-
-        if not os.path.isfile("data/2022/strat_card_ratings_pitcher.json"):
-            pitcher_sheet = "1NvSGtcSyReCiu8h9DPcm1ShYGCuzxucCzRIsXH6XOp4"
-            pitcher_range = "A:R"
-
-            pitchers = utils.get_sheet(pitcher_sheet, pitcher_range)
-
-            with open("data/2022/strat_card_ratings_pitcher.json", "w") as writefile:
-                writefile.write(json.dumps(pitchers))
-
-    def get_hitter_data(self, fresh=False):
-        if fresh:
-            os.system("rm -rf data/2022/strat_card_ratings_hitter.json")
-
-        if not os.path.isfile("data/2022/strat_card_ratings_hitter.json"):
-            hitter_sheet = "1Enmoqo6dgpFrP-IpEZbvIeDYrMwHLME2i0X3E8Z6dj8"
-            hitter_range = "a1:z1000"
-
-            hitters = utils.get_sheet(hitter_sheet, hitter_range)
-
-            with open("data/2022/strat_card_ratings_hitter.json", "w") as writefile:
-                writefile.write(json.dumps(hitters))
-
     def load_hitters(self, save=True):
-        with open("data/2022/strat_card_ratings_hitter.json", "r") as readfile:
-            hitters = json.loads(readfile.read())
+        with open(f"data/{self.season}/strat-hit.csv", "r") as readfile:
+            self.hitters = [h for h in csv.DictReader(readfile)]
 
-        for h in hitters:
+        self.hitter_obtb_l = [h['obtb_l'] for h in self.hitters]
+        self.hitter_obtb_r = [h['obtb_r'] for h in self.hitters]
+        self.hitter_obtb_tot = [h['tot'] for h in self.hitters]
+
+        for h in self.hitters:
             p = self.get_player(h)
-            for f in ["obtb", "ob", "h", "hr", "tb", "bb", "so"]:
-                for hand in ["l", "r"]:
-                    setattr(p, f"strat_{f}_{hand}", h[f"{f}_{hand}"])
-            print(p)
-
-            if save:
-                p.save()
-
-    def load_pitchers(self, save=True):
-        with open("data/2022/strat_card_ratings_pitcher.json", "r") as readfile:
-            pitchers = json.loads(readfile.read())
-
-        for h in pitchers:
-            p = self.get_player(h, hitter=False)
             if p:
-                for f in ["obtb", "ob", "h", "hr", "tb", "bb", "so"]:
-                    for hand in ["l", "r"]:
-                        setattr(p, f"strat_p_{f}_{hand}", h[f"{f}_{hand}"])
-            else:
-                print(f"{h['tm']}\t{h['player']}")
+                if not p.strat_ratings:
+                    # get us an empty dict
+                    p.strat_ratings = {}
 
-            if save:
-                p.save()
+                if not p.strat_ratings.get(self.season):
+                    # get us a current season and this specific year updated
+                    p.strat_ratings['current'] = {}
+                    p.strat_ratings[self.season] = {}
+
+                strat_dict = {}
+                strat_dict['unlimited'] = False
+                strat_dict['pa'] = int(h['pa'])
+
+                if strat_dict['pa'] >= 550:
+                    strat_dict['unlimited'] = True
+
+                strat_dict['obtb_l'] = h['obtb_l']
+                strat_dict['obtb_r'] = h['obtb_r']
+                strat_dict['obtb_tot'] = h['tot']
+
+                # strat_dict['obtb_l_ptile'] = stats.percentileofscore(self.hitter_obtb_l, h['obtb_l'], kind='strict')
+                # strat_dict['obtb_r_ptile'] = stats.percentileofscore(self.hitter_obtb_r, h['obtb_r'], kind='strict')
+                # strat_dict['obtb_tot_ptile'] = stats.percentileofscore(self.hitter_obtb_tot, h['tot'], kind='strict')
+
+                p.strat_ratings['current'] = strat_dict
+                p.strat_ratings[self.season] = strat_dict
+
+                if save:
+                    print(p)
+                    p.save()
+
+            else:
+                print(h)
