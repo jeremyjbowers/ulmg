34c2f998dd35f3da4663192ae8af5393b91c44bb
Jeremy Bowers <jeremyjbowers@gmail.com>
2023-01-28 16:57:37 -0500

    man, I gotta get these out of settings

--- config/dev/settings.py
+++ config/dev/settings.py
@@ -161,6 +161,8 @@ CSV_COLUMNS = [
 ]
 
 DRAFTS = [
+    {"title": "2023 Offseason AA", "url": "/draft/2023/offseason/aa/"},
+    {"title": "2023 Offseason Open", "url": "/draft/2023/offseason/open/"},
     {"title": "2022 Midseason AA", "url": "/draft/2022/midseason/aa/"},
     {"title": "2022 Midseason Open", "url": "/draft/2022/midseason/open/"},
     {"title": "2022 Offseason AA", "url": "/draft/2022/offseason/aa/"},
