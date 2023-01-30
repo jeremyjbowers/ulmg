2b98c6beb254f2c721e8f4f95cf73c7118609653
Jeremy Bowers <jeremyjbowers@gmail.com>
2023-01-29 19:55:15 -0500

    adds a static json management command

--- config/dev/settings.py
+++ config/dev/settings.py
@@ -90,8 +90,18 @@ LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "/accounts/login/"
 LOGOUT_REDIRECT_URL = "/"
 
+# STATICFILES
 STATIC_URL = "/static/"
 STATIC_ROOT = "static/"
+
+AWS_S3_REGION_NAME = "nyc3"
+AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
+AWS_DEFAULT_ACL = "public-read"
+AWS_STORAGE_BUCKET_NAME = "static-theulmg"
+AWS_S3_CUSTOM_DOMAIN = "static-theulmg.nyc3.cdn.digitaloceanspaces.com"
+AWS_LOCATION = "static"
 
 # ULMG SPECIFIC SETTINGS
 MLB_ROSTER_SIZE = 30
--- config/do_app_platform/settings.py
+++ config/do_app_platform/settings.py
@@ -22,15 +22,6 @@ DATABASES = {
     "default": dj_database_url.parse(DATABASE_URL),
 }
 
-AWS_S3_REGION_NAME = "nyc3"
-AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
-AWS_DEFAULT_ACL = "public-read"
-AWS_STORAGE_BUCKET_NAME = "static-theulmg"
-AWS_S3_CUSTOM_DOMAIN = "static-theulmg.nyc3.cdn.digitaloceanspaces.com"
-AWS_LOCATION = "static"
-
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
