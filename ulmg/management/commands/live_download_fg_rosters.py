87852f7e2766ad78105de1e6b491d1785cb20ea0
Jeremy Bowers <jeremyjbowers@gmail.com>
2023-09-11 11:51:36 -0400

    ugh frustruating

--- ulmg/management/commands/live_download_fg_rosters.py
+++ ulmg/management/commands/live_download_fg_rosters.py
@@ -1,7 +1,7 @@
 import csv
 import json
 import os
-from decimal import Decimal
+import time
 
 from bs4 import BeautifulSoup
 from dateutil.parser import parse
@@ -12,16 +12,26 @@ from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 import requests
+import urllib3
 
 from ulmg import models, utils
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        urllib3.disable_warnings()
+
         teams = settings.ROSTER_TEAM_IDS
 
         for team_id, team_abbrev, team_name in teams:
             url = f"https://cdn.fangraphs.com/api/depth-charts/roster?teamid={team_id}"
-            roster = requests.get(url, verify=False).json()
-            with open(f"data/rosters/{team_abbrev}_roster.json", "w") as writefile:
-                writefile.write(json.dumps(roster))
+
+            r = requests.get(url, verify=False)
+            print(r.status_code, url)
+            if r.status_code == 200:
+
+                roster = r.json()            
+                with open(f"data/rosters/{team_abbrev}_roster.json", "w") as writefile:
+                    writefile.write(json.dumps(roster))
+
+            time.sleep(5)
