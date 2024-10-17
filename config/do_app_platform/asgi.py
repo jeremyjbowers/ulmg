e37955ab3ee956246f3741b04cc586b6521d1c17
Jeremy Bowers <jeremyjbowers@gmail.com>
2024-10-17 13:12:44 +0200

    asgi for the website, lol

--- config/do_app_platform/asgi.py
+++ config/do_app_platform/asgi.py
@@ -2,5 +2,5 @@ import os
 settings_file = "config.do_app_platform.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_file)
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from django.core.asgi import get_asgi_application
+application = get_asgi_application()
