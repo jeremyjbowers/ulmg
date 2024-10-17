5f408276ad96fa259b452778812fbde2069af421
Jeremy Bowers <jeremyjbowers@gmail.com>
2024-10-17 13:06:15 +0200

    maybe asgi is faster than wsgi

--- config/dev/asgi.py
+++ config/dev/asgi.py
@@ -3,6 +3,6 @@ import os
 settings_file = "config.dev.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_file)
 
-from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
 
-application = get_wsgi_application()
+application = get_asgi_application()
