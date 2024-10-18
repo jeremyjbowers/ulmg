627dd359548dfce91fa2b8b764d2c68fb54eaeef
Jeremy Bowers <jeremyjbowers@gmail.com>
2024-10-18 11:41:01 +0200

    try db-backed sessions

--- config/dev/settings.py
+++ config/dev/settings.py
@@ -33,7 +33,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
 
 ROOT_URLCONF = "ulmg.urls"
 
