955e4e64f0ed020ff84c2ed9e877981fd02545d1
Jeremy Bowers <jeremyjbowers@gmail.com>
2025-03-24 17:52:07 +0100

    midseason mode

--- ulmg/views/my.py
+++ ulmg/views/my.py
@@ -23,18 +23,21 @@ def my_team(request):
     team = get_object_or_404(models.Team, owner_obj=context["owner"])
     return redirect(f"/teams/{ team.abbreviation }/")
 
+
 @login_required
-def my_draft_prep(request):
+def my_wishlist_beta(request):
     context = utils.build_context(request)
+
+    context['draft_type'] = "aa"
 
     context["team"] = get_object_or_404(models.Team, owner_obj=context["owner"])
     context["wishlist"] = models.Wishlist.objects.get(owner=context["owner"])
 
-    context['my_open_picks'] = models.DraftPick.objects.filter(team=context['team'], year=2025, season="offseason", draft_type="open")
-    context['all_open_picks'] = models.DraftPick.objects.filter(year=2025, season="offseason", draft_type="open").values('overall_pick_number', 'team__abbreviation')
+    context['my_aa_picks'] = models.DraftPick.objects.filter(team=context['team'], year=2025, season="offseason", draft_type="aa")
+    context['all_aa_picks'] = models.DraftPick.objects.filter(year=2025, season="offseason", draft_type="aa").values('overall_pick_number', 'team__abbreviation')
  
     context["players"] = models.WishlistPlayer.objects.filter(
-        wishlist=context["wishlist"], player__is_owned=False, player__level__in=["A","V"]
+        wishlist=context["wishlist"], player__is_owned=False, player__level="B"
     ).order_by("rank")
 
     context["tags"] = set()
@@ -47,186 +50,41 @@ def my_draft_prep(request):
     context["tags"] = sorted(list(context["tags"]), key=lambda x: x)
     context["num_owned"] = models.Player.objects.filter(team=context["team"]).count()
 
-    return render(request, "my/draft_prep.html", context)
+    return render(request, "my/wishlist_beta.html", context)
 
 
 @login_required
-def my_wishlist(request, list_type):
+def my_draft_prep(request, list_type):
     context = utils.build_context(request)
+
+    context['draft_type'] = "open"
+
     context["team"] = get_object_or_404(models.Team, owner_obj=context["owner"])
-    context["list_type"] = list_type
-    context['wishlist'] = models.Wishlist.objects.get(owner=context['owner'])
-    context["aa_hitters"] = []
-    context["aa_pitchers"] = []
-    context["op_hitters"] = []
-    context["op_pitchers"] = []
-    context["hitters"] = []
-    context["pitchers"] = []
+    context["wishlist"] = models.Wishlist.objects.get(owner=context["owner"])
 
-    if list_type == "draft":
-        for p in models.WishlistPlayer.objects.filter(wishlist=context["wishlist"]):
-            if not p.tier:
-                p.tier = 6
-            if not p.rank:
-                p.rank = 999
+    context['my_picks'] = models.DraftPick.objects.filter(team=context['team'], year=2025, season="offseason", draft_type="open")
+    context['all_picks'] = models.DraftPick.objects.filter(year=2025, season="offseason", draft_type="open").values('overall_pick_number', 'team__abbreviation')
+ 
+    context['players'] = []
+ 
+    if list_type == "offseason":    
+        context["players"] = models.WishlistPlayer.objects.filter(
+            wishlist=context["wishlist"], player__is_owned=False, player__level__in=["A","V"]
+        ).order_by("rank")
 
-            if not p.player.is_owned:
-                if p.player.level == "B":
-                    if p.player.position == "P":
-                        context["aa_pitchers"].append(p)
-                    else:
-                        context["aa_hitters"].append(p)
-                else:
-                    if p.player.position == "P":
-                        context["op_pitchers"].append(p)
-                    else:
-                        context["op_hitters"].append(p)
-        try:
-            context["aa_hitters"] = sorted(
-                context["aa_hitters"], key=lambda x: (x.tier, x.rank)
-            )
-        except:
-            pass
+    if list_type == "midseason":
+        context["players"] = models.WishlistPlayer.objects.filter(
+            wishlist=context["wishlist"], player__is_owned=False, player__level__in=["A","V"], player__is_carded=True
+        ).order_by("rank") 
 
-        try:
-            context["aa_pitchers"] = sorted(
-                context["aa_pitchers"], key=lambda x: (x.tier, x.rank)
-            )
-        except:
-            pass
+    context["tags"] = set()
 
-        context["op_hitters"] = sorted(
-            context["op_hitters"], key=lambda x: (x.tier, x.rank)
-        )
-        context["op_pitchers"] = sorted(
-            context["op_pitchers"], key=lambda x: (x.tier, x.rank)
-        )
+    for p in context["players"].values("tags"):
+        if p["tags"]:
+            for z in p["tags"]:
+                context["tags"].add(z)
 
-    if list_type == "trade":
-        for p in models.WishlistPlayer.objects.filter(wishlist=context["wishlist"]):
-            if p.player.is_owned and p.player.team != context["team"]:
-                if p.player.position == "P":
-                    context["pitchers"].append(p)
-                else:
-                    context["hitters"].append(p)
-
-        context["hitters"] = sorted(context["hitters"], key=lambda x: (x.tier, x.rank))
-        context["pitchers"] = sorted(
-            context["pitchers"], key=lambda x: (x.tier, x.rank)
-        )
-
-    return render(request, "my/wishlist.html", context)
-
-
-@login_required
-def my_wishlist_beta(request):
-    context = utils.build_context(request)
-    context["team"] = get_object_or_404(models.Team, owner_obj=context["owner"])
-    context['wishlist'] = models.Wishlist.objects.get(owner=context['owner'])
-
-    context["aa_hitters"] = []
-    context["aa_pitchers"] = []
-
-    for p in models.WishlistPlayer.objects.filter(wishlist=context["wishlist"]):
-        if not p.player.is_owned:
-            if p.player.level == "B":
-                if p.player.position == "P":
-                    context["aa_pitchers"].append(p)
-                else:
-                    context["aa_hitters"].append(p)
-            else:
-                if p.player.position == "P":
-                    context["op_pitchers"].append(p)
-                else:
-                    context["op_hitters"].append(p)
-    try:
-        context["aa_hitters"] = sorted(
-            context["aa_hitters"], key=lambda x: (x.tier, x.rank)
-        )
-    except:
-        pass
-
-    try:
-        context["aa_pitchers"] = sorted(
-            context["aa_pitchers"], key=lambda x: (x.tier, x.rank)
-        )
-    except:
-        pass
-
-    context["op_hitters"] = sorted(
-        context["op_hitters"], key=lambda x: (x.tier, x.rank)
-    )
-    context["op_pitchers"] = sorted(
-        context["op_pitchers"], key=lambda x: (x.tier, x.rank)
-    )
-
-    return render(request, "my/draft_prep.html", context)
-
-@login_required
-def my_midseason_draft(request):
-    context = utils.build_context(request)
-    context["team"] = get_object_or_404(models.Team, owner_obj=context["owner"])
-    context['wishlist'] = models.Wishlist.objects.get(owner=context['owner'])
+    context["tags"] = sorted(list(context["tags"]), key=lambda x: x)
     context["num_owned"] = models.Player.objects.filter(team=context["team"]).count()
 
-    if request.user.is_authenticated:
-        owner = models.Owner.objects.get(user=request.user)
-        if owner.team() == context["team"]:
-            context["own_team"] = True
-        else:
-            context["own_team"] = False
-    else:
-        context["own_team"] = False
-
-    context["op_hitters"] = models.WishlistPlayer.objects\
-    .filter(wishlist=context["wishlist"])\
-    .exclude(player__position="P")\
-    .filter(
-        Q(player__team__isnull=True, player__stats__2024_majors_hit__plate_appearances__gte=1)
-        | Q(
-            player__level="V",
-            player__is_owned=True,
-            player__is_mlb_roster=False,
-            player__is_1h_c=False,
-            player__is_1h_pos=False,
-            player__is_reserve=False,
-        )
-    )
-
-    context["op_pitchers"] = models.WishlistPlayer.objects\
-    .filter(wishlist=context["wishlist"])\
-    .filter(
-        Q(player__team__isnull=True, player__stats__2024_majors_pitch__ip__gte=1, player__position__icontains="P")
-        | Q(
-            player__level="V",
-            player__position="P",
-            player__is_owned=True,
-            player__is_mlb_roster=False,
-            player__is_1h_p=False,
-            player__is_reserve=False,
-        )
-    )
-
-    # context['op_hitters'] = models.WishlistPlayer.objects.exclude(player__position="P").filter(wishlist=context["wishlist"], player__is_owned=False, player__stats__2024_majors_hit__plate_appearances__gte=1)
-
-    # context['op_pitchers'] = models.WishlistPlayer.objects.filter(player__position="P", wishlist=context["wishlist"], player__is_owned=False, player__stats__2024_majors_pitch__g__gte=1)
-
-    context['aa_hitters'] = models.WishlistPlayer.objects.exclude(player__position="P").filter(wishlist=context["wishlist"], player__is_owned=False, player__is_carded=False, player__level="B")
-
-    context['aa_pitchers'] = models.WishlistPlayer.objects.filter(player__position="P", wishlist=context["wishlist"], player__is_owned=False, player__is_carded=False, player__level="B")
-
-    context["op_hitters"] = sorted(
-        context["op_hitters"], key=lambda x: (x.tier, x.rank)
-    )
-    context["op_pitchers"] = sorted(
-        context["op_pitchers"], key=lambda x: (x.tier, x.rank)
-    )
-
-    context["aa_hitters"] = sorted(
-        context["aa_hitters"], key=lambda x: (x.tier, x.rank)
-    )
-    context["aa_pitchers"] = sorted(
-        context["aa_pitchers"], key=lambda x: (x.tier, x.rank)
-    )
-
-    return render(request, "my/midseason_draft_prep.html", context)
+    return render(request, "my/wishlist_beta.html", context)
