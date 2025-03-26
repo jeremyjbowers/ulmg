439d19efe7cb740528c486aa0d312c395a745f5f
Jeremy Bowers <jeremyjbowers@gmail.com>
2025-03-26 10:55:40 +0100

    major stats refactor to show many years; still show latest in table views elsewhere

--- ulmg/management/commands/live_update_stats_from_fg_stats.py
+++ ulmg/management/commands/live_update_stats_from_fg_stats.py
@@ -263,7 +263,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.set_college_season()
-        # self.set_minor_season()
-        # self.set_mlb_hitter_season()
-        # self.set_mlb_pitcher_season()
+        self.set_minor_season()
+        self.set_mlb_hitter_season()
+        self.set_mlb_pitcher_season()
 
